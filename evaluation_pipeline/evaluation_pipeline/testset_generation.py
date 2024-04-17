dd788058a38c23236b4bedb3eb16393647e9f65d
Adrien <adrien.bertheleme@gmail.com>
2024-04-17 15:06:41 +0200

    add: testset generator

--- evaluation_pipeline/evaluation_pipeline/testset_generation.py
+++ evaluation_pipeline/evaluation_pipeline/testset_generation.py
@@ -1,36 +1,52 @@
-from ragas.testset.generator import TestsetGenerator
-from ragas.testset.evolutions import simple, reasoning, multi_context, conditional
-from langchain_openai import ChatOpenAI, OpenAIEmbeddings
-from langchain.document_loaders import DirectoryLoader
-from langchain_core.language_models import BaseLanguageModel
-from langchain_core.embeddings import Embeddings
-from ragas.embeddings import LangchainEmbeddingsWrapper, HuggingfaceEmbeddings
-from dotenv import load_dotenv, find_dotenv
 import os
+
+from dotenv import find_dotenv, load_dotenv
+from langchain.document_loaders import DirectoryLoader
+from langchain_openai import ChatOpenAI
+from ragas.embeddings import HuggingfaceEmbeddings, LangchainEmbeddingsWrapper
+from ragas.testset.evolutions import conditional, multi_context, reasoning, simple
+from ragas.testset.generator import TestsetGenerator
+from tqdm import tqdm
 
 load_dotenv(find_dotenv())
 
-
+openai = ChatOpenAI(model="gpt-3.5-turbo", api_key=os.getenv("OPENAI_KEY"))
 langchain_embeddings = HuggingfaceEmbeddings(model="OrdalieTech/Solon-embeddings-large-0.1")
-# langchain_embeddings = LangchainEmbeddingsWrapper(langchain_embeddings)
-loader = DirectoryLoader("your-directory")
+langchain_embeddings = LangchainEmbeddingsWrapper(langchain_embeddings)
+loader = DirectoryLoader("/home/adrien/Documents/Coding/RAG_ASN/Ingestion/ASN/txt")
 documents = loader.load()
 
+
+for document in tqdm(documents):
+    document.metadata["filename"] = document.metadata["source"]
+
+
 # generator with openai models
-generator_llm = ChatOpenAI(model="gpt-3.5-turbo", api_key=os.getenv("OPENAI_KEY"))
-critic_llm = ChatOpenAI(model="gpt-4", api_key=os.getenv("OPENAI_KEY"))
-embeddings = HuggingfaceEmbeddings
+generator_llm = openai
+critic_llm = openai
+embeddings = langchain_embeddings
 
 generator = TestsetGenerator.from_langchain(generator_llm, critic_llm, embeddings)
 
 # adapt to language
-language = "français"
+language = "french"
 
-generator.adapt(language, evolutions=[simple, reasoning, conditional, multi_context])
-generator.save(evolutions=[simple, reasoning, multi_context, conditional])
+generator.adapt(language, evolutions=[simple, reasoning, conditional, multi_context], cache_dir="./cache/french")
+generator.save(evolutions=[simple, reasoning, multi_context, conditional], cache_dir="./cache")
 
 testset = generator.generate_with_langchain_docs(
     documents,
     test_size=10,
-    distributions={simple: 0.5, reasoning: 0.25, multi_context: 0.25},
+    distributions={
+        simple: 0.4,
+        reasoning: 0.2,
+        multi_context: 0.2,
+        conditional: 0.2,
+    },
+    raise_exceptions=False,
+    is_async=False,
 )
+
+testset = testset.to_dataset()
+
+testset.push_to_hub(repo_id="AdrienB134/testset_asn_rag", api_key=os.environ["HF"])
